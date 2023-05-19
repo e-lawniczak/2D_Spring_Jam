27aae54a0ce6ab1d74265c0536f24f0b8f705678
Cryoxe <arthmal@icloud.com>
2023-05-19 22:37:31 +0200

    initSDL in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "common.h"
+#include "init.h"
 
 App app;
 int main(int argc, char* argv[]) {
 	std::cout << "Hello world;" << std::endl;
+	initSDL();
+	//Hacky way to make the window stay
+	SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 	return 0;
 }
